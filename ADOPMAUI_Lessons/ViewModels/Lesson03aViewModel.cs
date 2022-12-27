497c3c40d00b466ea01d6009c2608e11b8ed5384
Martin Lenart <martin_lenart@icloud.com>
2022-12-27 18:24:32 +0100

    update Lesson03

---
+++ ADOPMAUI_Lessons/ViewModels/Lesson03aViewModel.cs
@@ -0,0 +1,37 @@
+﻿using ADOPMAUI_Lessons.Views.Lesson03;
+using Microsoft.Maui.Controls;
+
+namespace ADOPMAUI_Lessons.ViewModels
+{
+    public class Lesson03aViewModel
+    {
+        public Lesson03aViewModel(Type type, string title, string description)
+        {
+            Type = type;
+            Title = title;
+            Description = description;
+        }
+
+        public Type Type { private set; get; }
+
+        public string Title { private set; get; }
+
+        public string Description { private set; get; }
+
+        static Lesson03aViewModel()
+        {
+            All = new List<Lesson03aViewModel>
+            {
+                
+                // Part 1. Getting Started with XAML
+                new Lesson03aViewModel(typeof(AbsoluteLayoutDemo), "AbsoluteLayoutDemo",
+                                      "Demonstrate the AbsoluteLayout"),
+
+                new Lesson03aViewModel(typeof(GridLayoutDemo), "GridLayoutDemo",
+                                      "Demonstrate the GridLayout")
+            };
+        }
+
+        public static IList<Lesson03aViewModel> All { private set; get; }
+    }
+}
---
+++ ADOPMAUI_Lessons/Views/Lesson03/GridLayoutDemo.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ADOPMAUI_Lessons.Views.Lesson03
+{
+	public partial class GridLayoutDemo : ContentPage
+	{
+		public GridLayoutDemo ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
